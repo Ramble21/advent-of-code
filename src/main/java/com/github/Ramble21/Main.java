85a8656ffc8903cc783ccdc0a13d77cfa52f9f6f
Ramble21 <zramble21@gmail.com>
2024-12-09 21:40:34 -0500

    day 4 parts 1 and 2

--- src/main/java/com/github/Ramble21/Main.java
+++ src/main/java/com/github/Ramble21/Main.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println(new Day3().solvePart1());
-        System.out.println(new Day3().solvePart2());
+        System.out.println(new Day4().solvePart1());
+        System.out.println(new Day4().solvePart2());
     }
 }
