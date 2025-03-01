797bc1bab516f46b5e5061e418f7a38f5024c103
Ramble21 <zramble21@gmail.com>
2025-02-28 20:36:33 -0500

    made day 24 part 1 less spaghetti

--- src/main/java/com/github/Ramble21/Day24.java
+++ src/main/java/com/github/Ramble21/Day24.java
@@ -1,77 +1,53 @@
 package com.github.Ramble21;
 
-import com.github.Ramble21.classes.days.LogicGate;
-import com.github.Ramble21.classes.days.LogicGateInstruction;
+import com.github.Ramble21.classes.days.Gate;
 
 import java.io.IOException;
 import java.util.*;
 
 public class Day24 extends DaySolver {
     private final List<String> input;
+    private final ArrayList<Gate> gates = new ArrayList<>();
+    private final HashMap<String, Integer> initialValues = new HashMap<>();
 
     public Day24() throws IOException {
         input = getInputLines(24);
+        for (String s : input){
+            if (s.contains(":")){
+                initialValues.put(s.substring(0, 3), Integer.parseInt(s.charAt(s.length()-1) + ""));
+            }
+            else if (!s.isEmpty()){
+                gates.add(new Gate(s));
+            }
+        }
     }
 
     public long solvePart1() throws IOException {
-        LogicGateInstruction[] instructions = null;
-        int i = 0;
-        for (int j = 0; j < input.size(); j++) {
-            String s = input.get(j);
-            if (s.length() == 6) {
-                new LogicGate(s.substring(0, 3), Integer.parseInt(s.substring(5)));
-            } else if (s.length() > 6) {
-                assert instructions != null;
-                instructions[i] = new LogicGateInstruction(s);
-                i++;
-            } else {
-                instructions = new LogicGateInstruction[input.size() - j - 1];
+        HashMap<String, Integer> results = new HashMap<>();
+        LinkedList<Gate> gateQueue = new LinkedList<>(gates);
+        while (!gateQueue.isEmpty()){
+            Gate current = gateQueue.pollFirst();
+            if (results.containsKey(current.getInputOne()) && results.containsKey(current.getInputTwo())){
+                results.put(current.getOutput(), current.getResultOfRunning(results.get(current.getInputOne()), results.get(current.getInputTwo())));
+            }
+            else if (initialValues.containsKey(current.getInputOne()) && initialValues.containsKey(current.getInputTwo())){
+                results.put(current.getOutput(), current.getResultOfRunning(initialValues.get(current.getInputOne()), initialValues.get(current.getInputTwo())));
+            }
+            else{
+                gateQueue.addLast(current);
             }
         }
-        assert instructions != null;
-        while (containsNull(instructions)) {
-            for (LogicGateInstruction instruction : instructions) {
-                if (instruction.getResult() == null) instruction.run();
-            }
+        String currentGateNo = "z00";
+        StringBuilder result = new StringBuilder();
+        while (results.containsKey(currentGateNo)){
+            result.append(results.get(currentGateNo));
+            currentGateNo = "z" + (Integer.parseInt(currentGateNo.substring(1)) + 1);
+            if (currentGateNo.length() == 2) currentGateNo = "z0" + currentGateNo.substring(currentGateNo.length()-1);
         }
-
-        ArrayList<String> zGates = new ArrayList<>();
-        HashMap<String, Integer> map = LogicGate.getLogicGates();
-        for (String identifier : map.keySet()) {
-            if (identifier.startsWith("z")) zGates.add(identifier);
-        }
-        zGates.sort((s1, s2) -> {
-            int num1 = Integer.parseInt(s1.substring(1));
-            int num2 = Integer.parseInt(s2.substring(1));
-            return Integer.compare(num2, num1);
-        });
-        StringBuilder binaryNum = new StringBuilder();
-        for (String x : zGates) {
-            binaryNum.append(map.get(x));
-        }
-        return binaryToLong(binaryNum.toString());
+        return Long.parseLong(result.reverse().toString(), 2);
     }
 
     public long solvePart2() throws IOException {
         return 0;
-    }
-
-    public boolean containsNull(LogicGateInstruction[] arr) {
-        for (LogicGateInstruction l : arr) {
-            if (l.getResult() == null) return true;
-        }
-        return false;
-    }
-
-    public long binaryToLong(String binary) {
-        long result = 0;
-        for (int i = 0; i < binary.length(); i++) {
-            if (binary.charAt(i) == '1') {
-                result = (result << 1) | 1;
-            } else {
-                result = result << 1;
-            }
-        }
-        return result;
     }
 }
