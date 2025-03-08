539207d96a0ce38415acfd2c69fad8b13c5844bc
Ramble21 <zramble21@gmail.com>
2025-03-07 20:56:40 -0500

    day 11 fix / cleanup

--- src/main/java/com/github/Ramble21/Day11.java
+++ src/main/java/com/github/Ramble21/Day11.java
@@ -1,63 +1,59 @@
 package com.github.Ramble21;
 
 import java.io.IOException;
-import java.util.Arrays;
+import java.sql.SQLOutput;
 import java.util.HashMap;
 import java.util.List;
 
 public class Day11 extends DaySolver{
-    private final List<String> input;
     private final HashMap<Long, Long> frequencyMap = new HashMap<>();
     public Day11() throws IOException {
-        input = getInputLines(11);
+        List<String> input = getInputLines(11);
+        for (String s : input.get(0).split(" ")){
+            long startingNum = Long.parseLong(s);
+            frequencyMap.put(startingNum, 1 + frequencyMap.getOrDefault(startingNum, 0L));
+        }
     }
     public long solvePart1() throws IOException {
-        String longAssString = input.get(0);
-        long[] initial = Arrays.stream(longAssString.split(" ")).mapToLong(Long::parseLong).toArray();
-        initializeHashmap(initial);
         for (int i = 0; i < 25; i++){
             blink();
         }
-        return amountOfStones();
+        long sum = 0;
+        for (long value : frequencyMap.values()){
+            sum += value;
+        }
+        return sum;
     }
-    public long solvePart2() {
-        String longAssString = input.get(0);
-        long[] initial = Arrays.stream(longAssString.split(" ")).mapToLong(Long::parseLong).toArray();
-        initializeHashmap(initial);
-        for (int i = 0; i < 75; i++){
+    public long solvePart2() throws IOException {
+        for (int i = 0; i < 50; i++){
             blink();
         }
-        return amountOfStones();
+        long sum = 0;
+        for (long value : frequencyMap.values()){
+            sum += value;
+        }
+        return sum;
     }
-    public void blink(){
-        HashMap<Long, Long> updatedMap = new HashMap<>();
-        for (long key : frequencyMap.keySet()){
-            long newKey;
-            long newKey2 = -1;
-            if (key == 0) newKey = 1;
-            else if (Long.toString(key).length() % 2 == 0){
-                String toS = Long.toString(key);
-                newKey = Long.parseLong(toS.substring(0, toS.length()/2));
-                newKey2 = Long.parseLong(toS.substring(toS.length()/2));
+    private void blink() {
+        HashMap<Long, Long> temp = new HashMap<>();
+        for (long stone : frequencyMap.keySet()){
+            long count = frequencyMap.get(stone);
+            int numDigits = (int)Math.log10(stone) + 1;
+            if (stone == 0){
+                temp.put(1L, count + temp.getOrDefault(1L, 0L));
             }
-            else newKey = key * 2024;
-
-            updatedMap.put(newKey, (updatedMap.getOrDefault(newKey, 0L) + frequencyMap.get(key)));
-            if (newKey2 != -1) updatedMap.put(newKey2, (updatedMap.getOrDefault(newKey2, 0L) + frequencyMap.get(key)));
+            else if (numDigits % 2 == 0){
+                int divider = (int) Math.pow(10, (double) numDigits / 2);
+                long firstHalf = stone / divider;
+                long secondHalf = stone % divider;
+                temp.put(firstHalf, count + temp.getOrDefault(firstHalf, 0L));
+                temp.put(secondHalf, count + temp.getOrDefault(secondHalf, 0L));
+            }
+            else {
+                temp.put(stone * 2024, count + temp.getOrDefault(stone * 2024, 0L));
+            }
         }
         frequencyMap.clear();
-        frequencyMap.putAll(updatedMap);
-    }
-    public void initializeHashmap(long[] initial){
-        for (long l : initial) {
-           frequencyMap.put(l, frequencyMap.getOrDefault(l, 0L)+1);
-        }
-    }
-    public long amountOfStones(){
-        long count = 0;
-        for (long key : frequencyMap.keySet()){
-            count += frequencyMap.get(key);
-        }
-        return count;
+        frequencyMap.putAll(temp);
     }
 }
