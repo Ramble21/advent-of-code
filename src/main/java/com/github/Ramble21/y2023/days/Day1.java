45fce0f8eb2ee8f590c6121a8656c7c9d274dcd3
Ramble21 <zramble21@gmail.com>
2025-03-11 15:06:42 -0400

    2023 day 1 part 2

--- src/main/java/com/github/Ramble21/y2023/days/Day1.java
+++ src/main/java/com/github/Ramble21/y2023/days/Day1.java
@@ -32,6 +32,45 @@ public class Day1 extends DaySolver {
     }
 
     public long solvePart2() throws IOException {
-        return 0;
+        long total = 0;
+        for (String s : input){
+            StringBuilder calibrationValue = new StringBuilder();
+            for (int i = 0; i < s.length(); i++){
+                if (Character.isDigit(s.charAt(i))) {
+                    calibrationValue.append(s.charAt(i));
+                    break;
+                }
+                int wordNumber = numberAsWord(s, i);
+                if (wordNumber != -1){
+                    calibrationValue.append(wordNumber);
+                    break;
+                }
+            }
+            for (int i = s.length() - 1; i >= 0; i--){
+                if (Character.isDigit(s.charAt(i))){
+                    calibrationValue.append(s.charAt(i));
+                    break;
+                }
+                int wordNumber = numberAsWord(s, i);
+                if (wordNumber != -1){
+                    calibrationValue.append(wordNumber);
+                    break;
+                }
+            }
+            total += Integer.parseInt(calibrationValue.toString());
+        }
+        return total;
+    }
+    private int numberAsWord(String s, int startingIndex){
+        if (s.substring(startingIndex).startsWith("one")) return 1;
+        if (s.substring(startingIndex).startsWith("two")) return 2;
+        if (s.substring(startingIndex).startsWith("three")) return 3;
+        if (s.substring(startingIndex).startsWith("four")) return 4;
+        if (s.substring(startingIndex).startsWith("five")) return 5;
+        if (s.substring(startingIndex).startsWith("six")) return 6;
+        if (s.substring(startingIndex).startsWith("seven")) return 7;
+        if (s.substring(startingIndex).startsWith("eight")) return 8;
+        if (s.substring(startingIndex).startsWith("nine")) return 9;
+        return -1;
     }
 }
