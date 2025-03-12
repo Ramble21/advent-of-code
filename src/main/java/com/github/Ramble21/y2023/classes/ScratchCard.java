a51a0a8a7477e6ea53dcc1a4af29992e00fdee03
Ramble21 <zramble21@gmail.com>
2025-03-12 15:55:41 -0400

    2023 day 4 part 2

--- src/main/java/com/github/Ramble21/y2023/classes/ScratchCard.java
+++ src/main/java/com/github/Ramble21/y2023/classes/ScratchCard.java
@@ -1,16 +1,19 @@
 package com.github.Ramble21.y2023.classes;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class ScratchCard {
-    private final int ID;
     private final int[] winningNumbers;
     private final int[] actualNumbers;
+    private static final HashMap<Integer, Integer> numCardsWon = new HashMap<>();
+
     public ScratchCard(String inputLine) {
         // 2023: Advent of Parsing
-        ID = Integer.parseInt(inputLine.substring(inputLine.indexOf(" ") + 1, inputLine.indexOf(":")).trim());
+        int ID = Integer.parseInt(inputLine.substring(inputLine.indexOf(" ") + 1, inputLine.indexOf(":")).trim());
         winningNumbers = Arrays.stream(inputLine.substring(inputLine.indexOf(":") + 1, inputLine.indexOf("|")).trim().split("\\s+")).mapToInt(Integer::parseInt).toArray();
         actualNumbers = Arrays.stream(inputLine.substring(inputLine.indexOf("|") + 1).trim().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+        numCardsWon.put(ID, getNumMatches());
     }
     public int getPoints(){
         int count = 0;
@@ -24,5 +27,17 @@ public class ScratchCard {
             }
         }
         return count;
+    }
+    public int getNumMatches() {
+        int count = 0;
+        for (int i : actualNumbers){
+            for (int j : winningNumbers){
+                if (i == j) count++;
+            }
+        }
+        return count;
+    }
+    public static HashMap<Integer, Integer> getNumCardsWon() {
+        return numCardsWon;
     }
 }
--- src/main/java/com/github/Ramble21/y2023/days/Day4.java
+++ src/main/java/com/github/Ramble21/y2023/days/Day4.java
@@ -4,6 +4,8 @@ import com.github.Ramble21.DaySolver;
 import com.github.Ramble21.y2023.classes.ScratchCard;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class Day4 extends DaySolver {
@@ -20,6 +22,35 @@ public class Day4 extends DaySolver {
     }
 
     public long solvePart2() throws IOException {
-        return 0;
+        HashMap<Integer, Integer> numCardsWon = ScratchCard.getNumCardsWon();
+        int maxNum = 0;
+        long count = 0;
+        for (int n : numCardsWon.keySet()){
+            maxNum = Math.max(n, maxNum);
+        }
+        for (int x : numCardsWon.keySet()){
+            count += addNewCards(x, maxNum, numCardsWon);
+        }
+        return count;
+    }
+    private final HashMap<Integer, Integer> memo = new HashMap<>();
+    public int addNewCards(int ID, int max, HashMap<Integer, Integer> numCardsWon){
+        if (memo.containsKey(ID)){
+            return memo.get(ID);
+        }
+        if (ID > max){
+            memo.put(ID, 0);
+            return 0;
+        }
+        if (ID == max){
+            memo.put(ID, 1);
+            return 1;
+        }
+        int result = 1;
+        for (int i = 1; i <= numCardsWon.get(ID); i++){
+            result += addNewCards(ID + i, max, numCardsWon);
+        }
+        memo.put(ID, result);
+        return result;
     }
 }
