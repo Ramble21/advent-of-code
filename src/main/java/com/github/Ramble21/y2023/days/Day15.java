b9195e47b01f30e016cb5dd1659dfe6c4961908c
Ramble21 <zramble21@gmail.com>
2025-03-24 20:00:00 -0400

    2023 day 15 part 2

--- src/main/java/com/github/Ramble21/y2023/days/Day15.java
+++ src/main/java/com/github/Ramble21/y2023/days/Day15.java
@@ -3,24 +3,56 @@ package com.github.Ramble21.y2023.days;
 import com.github.Ramble21.DaySolver;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.*;
 
 public class Day15 extends DaySolver {
     private final List<String> input;
+    private final String[] references;
+    private final int[] values;
     public Day15() throws IOException {
         input = getInputLines(2023, 15);
+        references = input.get(0).split(",");
+        values = new int[references.length];
     }
     public long solvePart1() throws IOException {
-        String[] sussy = input.get(0).split(",");
         int total = 0;
-        for (String s : sussy) {
-            total += hash(s);
+        for (int i = 0; i < references.length; i++) {
+            total += hash(references[i]);
+            values[i] = hash(getHeader(references[i]));
         }
         return total;
     }
 
     public long solvePart2() throws IOException {
-        return 0;
+        HashMap<Integer, LinkedList<String>> boxes = new HashMap<>();
+        HashMap<String, Integer> focalLengths = new HashMap<>();
+        for (int i = 0; i < values.length; i++) {
+            String lens = references[i];
+            String lensHeader = getHeader(lens);
+            int boxNo = values[i];
+            if (lens.charAt(lens.length() - 1) == '-') {
+                if (boxes.containsKey(boxNo)) {
+                    boxes.get(boxNo).remove(lensHeader);
+                }
+            }
+            else {
+                focalLengths.put(lensHeader, Integer.parseInt(lens.substring(lens.indexOf("=") + 1)));
+
+                LinkedList<String> box = boxes.getOrDefault(boxNo, new LinkedList<>());
+                boolean skibidiToilet = box.contains(lensHeader);
+                if (!skibidiToilet) box.addLast(lensHeader);
+                boxes.put(boxNo, box);
+            }
+        }
+        int focusingPower = 0;
+        for (Map.Entry<Integer, LinkedList<String>> box : boxes.entrySet()) {
+            int boxNo = box.getKey();
+            LinkedList<String> sussy = box.getValue();
+            for (int i = 0; i < sussy.size(); i++) {
+                focusingPower += (1 + boxNo) * (1 + i) * focalLengths.get(sussy.get(i));
+            }
+        }
+        return focusingPower;
     }
 
     private int hash(String s) {
@@ -30,5 +62,11 @@ public class Day15 extends DaySolver {
             current = (current * 17) % 256;
         }
         return current;
+    }
+    private String getHeader(String s) {
+        if (s.contains("=")) {
+            return s.substring(0, s.indexOf("="));
+        }
+        return s.substring(0, s.indexOf("-"));
     }
 }
