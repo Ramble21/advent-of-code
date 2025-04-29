af011c2df901ade37892b1ec575748b554140d10
Ramble21 <zramble21@gmail.com>
2025-04-29 19:56:46 -0400

    2023 day 20 part 2 finally lol

--- src/main/java/com/github/Ramble21/y2023/classes/PulseModule.java
+++ src/main/java/com/github/Ramble21/y2023/classes/PulseModule.java
@@ -1,5 +1,7 @@
 package com.github.Ramble21.y2023.classes;
 
+import com.github.Ramble21.y2023.days.Day20;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -10,7 +12,7 @@ public class PulseModule {
     private final char prefix;
 
     private boolean flipFlopState;
-    private HashMap<String, Boolean> mostRecentPulses = new HashMap<>();
+    private HashMap<String, Boolean> mostRecentPulses;
 
     public PulseModule(String inputLine) {
         String[] parsed = inputLine.split("[\\s,->]+");
@@ -19,11 +21,41 @@ public class PulseModule {
         this.destinations = Arrays.copyOfRange(parsed, 1, parsed.length);
         if (prefix == '%') flipFlopState = false;
     }
+
+    public void reset() {
+        mostRecentPulses.clear();
+        flipFlopState = false;
+    }
     public String getIdentifier() {
         return identifier;
     }
     public String[] getDestinations() {
         return destinations;
+    }
+    public char getPrefix() {
+        return prefix;
+    }
+    public HashMap<String, Boolean> getMostRecentPulses() {
+        return mostRecentPulses;
+    }
+    public boolean onlyRemembersHighPulses() {
+        if (prefix == '%') return false;
+        return mostRecentPulses.containsValue(false);
+    }
+    public void initializeRequirements(PulseModule[] modules) {
+        ArrayList<PulseModule> result = new ArrayList<>();
+        for (PulseModule p : modules) {
+            for (String destination : p.getDestinations()) {
+                if (destination.equals(identifier)) {
+                    result.add(p);
+                    break;
+                }
+            }
+        }
+        mostRecentPulses = new HashMap<>();
+        for (PulseModule requirement : result) {
+            mostRecentPulses.put(requirement.identifier, false);
+        }
     }
     private Boolean getOutputPulse(boolean input, String inputFrom, PulseModule[] modules) {
         if (prefix == 'b') {
@@ -61,5 +93,8 @@ public class PulseModule {
             if (p.identifier.equals(goal)) return p;
         }
         return null;
+    }
+    public String toString() {
+        return "" + (prefix == '&' ? (onlyRemembersHighPulses() ? 1 : 0) : (flipFlopState ? 1 : 0)) + prefix + identifier + "->" + Arrays.toString(getDestinations());
     }
 }
--- src/main/java/com/github/Ramble21/y2023/days/Day20.java
+++ src/main/java/com/github/Ramble21/y2023/days/Day20.java
@@ -5,9 +5,7 @@ import com.github.Ramble21.y2023.classes.Pulse;
 import com.github.Ramble21.y2023.classes.PulseModule;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class Day20 extends DaySolver {
     private final PulseModule[] modules;
@@ -18,10 +16,9 @@ public class Day20 extends DaySolver {
         for (PulseModule module : modules) {
             if (module.getIdentifier().equals("broadcaster")) {
                 broadcaster = module;
-                break;
             }
+            module.initializeRequirements(modules);
         }
-        assert broadcaster != null;
         this.broadcaster = broadcaster;
     }
     public long solvePart1() throws IOException {
@@ -34,7 +31,90 @@ public class Day20 extends DaySolver {
         return totalLow * totalHigh;
     }
     public long solvePart2() throws IOException {
-        return 0;
+        return numButtonPressesNeeded("rx");
+    }
+    public long numButtonPressesNeeded(String moduleName) {
+        ArrayList<PulseModule> requirements = getRequirementsOf(moduleName);
+        boolean onlyContainsConjunctions = requirements.stream()
+                .noneMatch(r -> r.getPrefix() == '%');
+        if (onlyContainsConjunctions) {
+            if (requirements.size() == 1) {
+                return numButtonPressesNeeded(requirements.get(0).getIdentifier());
+            }
+            else {
+                long[] count = new long[requirements.size()];
+                for (int i = 0; i < requirements.size(); i++) {
+                    count[i] = numButtonPressesNeeded(requirements.get(i).getIdentifier());
+                }
+                return Day8.lcmOfArray(count);
+            }
+        }
+        return binaryCounterLength(moduleName);
+    }
+    public long binaryCounterLength(String conjunctionModName) {
+        ArrayList<PulseModule> counterChain = getCounterChain(conjunctionModName);
+        StringBuilder binaryCount = new StringBuilder();
+        for (PulseModule p : counterChain) {
+            char binaryResult = '0';
+            for (String s : p.getDestinations()) {
+                if (s.equals(conjunctionModName)) {
+                    binaryResult = '1';
+                    break;
+                }
+            }
+            binaryCount.append(binaryResult);
+        }
+        binaryCount.reverse();
+        System.out.println(binaryCount);
+        return Integer.parseInt(binaryCount.toString(), 2);
+    }
+    public ArrayList<PulseModule> getCounterChain(String conjunctionModName) {
+        ArrayList<PulseModule> result = getRequirementsOf(conjunctionModName);
+        PulseModule conjunction = PulseModule.getModuleByName(conjunctionModName, modules);
+        assert conjunction != null;
+        for (String s : conjunction.getDestinations()) {
+            PulseModule mod = PulseModule.getModuleByName(s, modules);
+            assert mod != null;
+            if (mod.getPrefix() == '%') {
+                result.add(PulseModule.getModuleByName(s, modules));
+            }
+        }
+        result.sort(Comparator.comparingInt(conj -> numberStepsFromBroadcaster(conj.getIdentifier())));
+        result.remove(0);
+        System.out.println(result);
+        return result;
+    }
+    public String generateSequence(ArrayList<PulseModule> modules) {
+        StringBuilder result = new StringBuilder();
+        for (PulseModule m : modules) {
+            result.append(m.toString().charAt(0));
+        }
+        return result.reverse().toString();
+    }
+    public ArrayList<PulseModule> getRequirementsOf(String moduleName) {
+        ArrayList<PulseModule> result = new ArrayList<>();
+        for (PulseModule p : modules) {
+            for (String destination : p.getDestinations()) {
+                if (destination.equals(moduleName)) {
+                    result.add(p);
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+    public int numberStepsFromBroadcaster(String moduleIdentifier) {
+        for (String s : broadcaster.getDestinations()) {
+            if (s.equals(moduleIdentifier)) {
+                return 1;
+            }
+        }
+        for (PulseModule p : getRequirementsOf(moduleIdentifier)) {
+            if (p.getPrefix() == '%') {
+                return 1 + numberStepsFromBroadcaster(p.getIdentifier());
+            }
+        }
+        throw new RuntimeException(moduleIdentifier);
     }
     public long[] pushButton() {
         long low = broadcaster.getDestinations().length + 1, high = 0;
@@ -50,5 +130,10 @@ public class Day20 extends DaySolver {
             pulses.addAll(result);
         }
         return new long[]{low, high};
+    }
+    public void resetModules() {
+        for (PulseModule m : modules) {
+            m.reset();
+        }
     }
 }
