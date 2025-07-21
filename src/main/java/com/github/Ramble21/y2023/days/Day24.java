72e8eb4a6f7b11ba724a021cd4e37b5702cdd91d
Ramble21 <zramble21@gmail.com>
2025-07-20 21:03:53 -0400

    2023 day 24 part 2

--- src/main/java/com/github/Ramble21/y2023/days/Day24.java
+++ src/main/java/com/github/Ramble21/y2023/days/Day24.java
@@ -2,6 +2,8 @@ package com.github.Ramble21.y2023.days;
 
 import com.github.Ramble21.DaySolver;
 import com.github.Ramble21.y2024.classes.Hailstone;
+import java.util.*;
+import org.apache.commons.math3.linear.*;
 
 import java.io.IOException;
 
@@ -43,7 +45,53 @@ public class Day24 extends DaySolver {
         boolean yInFuture = hailstone.getVelocityVector()[1] > 0 ? y > hailstone.getPositionVector()[1] : y < hailstone.getPositionVector()[1];
         return xInFuture && yInFuture;
     }
-    public long solvePart2() throws IOException {
-        return 0;
+    public long solvePart2() {
+        // i basically just copied this logic and switched it to java i have no idea how to do the linear algebra
+        // https://pastebin.com/NmR6ZDXL
+        Hailstone h0 = hailstones[0];
+        Hailstone h1 = hailstones[1];
+        Hailstone h2 = hailstones[2];
+
+        double[] p0 = Arrays.stream(h0.getPositionVector()).mapToDouble(x -> x).toArray();
+        double[] p1 = Arrays.stream(h1.getPositionVector()).mapToDouble(x -> x).toArray();
+        double[] p2 = Arrays.stream(h2.getPositionVector()).mapToDouble(x -> x).toArray();
+
+        double[] v0 = Arrays.stream(h0.getVelocityVector()).asDoubleStream().toArray();
+        double[] v1 = Arrays.stream(h1.getVelocityVector()).asDoubleStream().toArray();
+        double[] v2 = Arrays.stream(h2.getVelocityVector()).asDoubleStream().toArray();
+
+        Array2DRowRealMatrix M = new Array2DRowRealMatrix(6, 6);
+        ArrayRealVector rhs = new ArrayRealVector(6);
+
+        RealVector p0cv0 = cross(p0, v0);
+        RealVector p1cv1 = cross(p1, v1);
+        RealVector p2cv2 = cross(p2, v2);
+
+        rhs.setSubVector(0, p1cv1.subtract(p0cv0));
+        rhs.setSubVector(3, p2cv2.subtract(p0cv0));
+
+        M.setSubMatrix(crossMatrix(v0).subtract(crossMatrix(v1)).getData(), 0, 0);
+        M.setSubMatrix(crossMatrix(v0).subtract(crossMatrix(v2)).getData(), 3, 0);
+        M.setSubMatrix(crossMatrix(p1).subtract(crossMatrix(p0)).getData(), 0, 3);
+        M.setSubMatrix(crossMatrix(p2).subtract(crossMatrix(p0)).getData(), 3, 3);
+
+        RealVector solution = new LUDecomposition(M).getSolver().solve(rhs);
+        RealVector rockPos = solution.getSubVector(0, 3);
+
+        return Math.round(rockPos.getEntry(0)) + Math.round(rockPos.getEntry(1)) + Math.round(rockPos.getEntry(2));
+    }
+    public static Array2DRowRealMatrix crossMatrix(double[] v) {
+        return new Array2DRowRealMatrix(new double[][] {
+                {0, -v[2], v[1]},
+                {v[2], 0, -v[0]},
+                {-v[1], v[0], 0}
+        });
+    }
+    public static RealVector cross(double[] a, double[] b) {
+        return new ArrayRealVector(new double[] {
+                a[1] * b[2] - a[2] * b[1],
+                a[2] * b[0] - a[0] * b[2],
+                a[0] * b[1] - a[1] * b[0]
+        });
     }
 }
