7306afc4fa91a54cd33c2aa503f5231d9e8fc26d
Ramble21 <zramble21@gmail.com>
2025-12-06 00:34:53 -0500

    2025 day 6 part 2

--- python/y2025/day6.py
+++ python/y2025/day6.py
@@ -23,7 +23,18 @@ def solve_part1(p_input):
     return summ
 
 def solve_part2(p_input):
-    pass
+    summ = 0
+    last_space = -1
+    for c in range(len(p_input[0])):
+        all_spaces = True
+        for r in range(len(p_input)):
+            if p_input[r][c] != ' ':
+                all_spaces = False
+                break
+        if all_spaces:
+            summ += solve_math_2(p_input, last_space + 1, c - 1)
+            last_space = c
+    return summ
 
 def solve_math(p_input, first_col, last_col):
     is_mult = "*" in p_input[-1][first_col:last_col+1]
@@ -37,6 +48,26 @@ def solve_math(p_input, first_col, last_col):
     summ = 0
     for i in range(len(p_input) - 1):
         lc_num = p_input[i][first_col:last_col + 1]
+        num = int("".join(lc_num).strip())
+        summ += num
+    return summ
+
+def solve_math_2(p_input, first_col, last_col):
+    is_mult = "*" in p_input[-1][first_col:last_col + 1]
+    if is_mult:
+        prod = 1
+        for i in range(last_col, first_col - 1, -1):
+            lc_num = []
+            for j in range(len(p_input) - 1):
+                lc_num.append(p_input[j][i])
+            num = int("".join(lc_num).strip())
+            prod *= num
+        return prod
+    summ = 0
+    for i in range(last_col, first_col - 1, -1):
+        lc_num = []
+        for j in range(len(p_input) - 1):
+            lc_num.append(p_input[j][i])
         num = int("".join(lc_num).strip())
         summ += num
     return summ
