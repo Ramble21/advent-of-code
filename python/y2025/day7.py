3b762f19e40cb014eaec92511b58606a08b122f5
Ramble21 <zramble21@gmail.com>
2025-12-07 11:23:36 -0500

    2025 day 7 part 2

--- python/y2025/day7.py
+++ python/y2025/day7.py
@@ -1,13 +1,12 @@
+import functools
+
 from python.helper_classes.common_methods import load_grid
 
 def parse_data():
     return load_grid(2025, 7)
 
 def solve_part1(p_input):
-    start_loc = "".join(p_input[0]).find("S")
-    beams = [start_loc]
-    r = 0
-    num_splits = 0
+    beams, r, num_splits = ["".join(p_input[0]).find("S")], 0, 0
     while r + 1 < len(p_input):
         new_beams = []
         for beam in beams:
@@ -22,4 +21,11 @@ def solve_part1(p_input):
     return num_splits
 
 def solve_part2(p_input):
-    pass
+    @functools.cache
+    def num_tls(r, c):
+        while r + 1 < len(p_input) and p_input[r][c] != '^':
+            r += 1
+        if r + 1 == len(p_input):
+            return 1
+        return num_tls(r, c - 1) + num_tls(r, c + 1)
+    return num_tls(0, "".join(p_input[0]).find("S"))
