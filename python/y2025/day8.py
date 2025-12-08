902ead6db9af19d686a9b33264983106dcfa3e03
Ramble21 <zramble21@gmail.com>
2025-12-08 16:31:20 -0500

    2025 day 8 part 2

--- python/y2025/day8.py
+++ python/y2025/day8.py
@@ -8,20 +8,33 @@ from python.helper_classes.common_methods import load_lines
 def parse_data():
     strs = load_lines(2025, 8)
     coord_tuples = list((int(x), int(y), int(z)) for line in strs for x,y,z in [re.split(",", line)])
-    return coord_tuples
+    circuit_distances = []
+    for i in range(len(coord_tuples)):
+        for j in range(i + 1, len(coord_tuples)):
+            dx, dy, dz = (x1 := coord_tuples[i][0]) - (x2 := coord_tuples[j][0]), (y1 := coord_tuples[i][1]) - (y2 := coord_tuples[j][1]), (
+                z1 := coord_tuples[i][2]) - (z2 := coord_tuples[j][2])
+            dist = math.sqrt(dx ** 2 + dy ** 2 + dz ** 2)
+            circuit_distances.append((x1, y1, z1, x2, y2, z2, dist))
+    return sorted(circuit_distances, key=lambda duo: duo[6])
 
 def solve_part1(p_input):
     n = 1000
-    circuit_distances = []
-    for i in range(len(p_input)):
-        for j in range(i + 1, len(p_input)):
-            dx, dy, dz = (x1 := p_input[i][0]) - (x2 := p_input[j][0]), (y1 := p_input[i][1]) - (y2 := p_input[j][1]), (z1 := p_input[i][2]) - (z2 := p_input[j][2])
-            dist = math.sqrt(dx ** 2 + dy ** 2 + dz ** 2)
-            circuit_distances.append((x1, y1, z1, x2, y2, z2, dist))
-    top_x = heapq.nsmallest(n, circuit_distances, key=lambda duo: duo[6])
-    circuits, next_circuit_id = {}, 0
+    circuits, _, _ = add_circuits(p_input[:n], 0)
+    largest_three = [length for _, length in Counter(circuits.values()).most_common(3)]
+    return largest_three[0] * largest_three[1] * largest_three[2]
+
+def solve_part2(p_input):
+    circuits, next_circuit_id, last_two = add_circuits(p_input, 0)
+    while len(Counter(circuits.values())) > 1:
+        circuits, next_circuit_id, last_two = add_circuits(p_input, next_circuit_id)
+    return last_two[0][0] * last_two[1][0]
+
+def add_circuits(top_x, next_circuit_id):
+    circuits = {}
+    last_two = None
     for dist in top_x:
         box1, box2 = (dist[0], dist[1], dist[2]), (dist[3], dist[4], dist[5])
+        l2 = (box1, box2)
         if box1 not in circuits and box2 not in circuits:
             circuits[box1], circuits[box2] = next_circuit_id, next_circuit_id
             next_circuit_id += 1
@@ -34,8 +47,8 @@ def solve_part1(p_input):
                 if v == circuits[box2] and k != box2:
                     circuits[k] = circuits[box1]
             circuits[box2] = circuits[box1]
-    largest_three = [length for _, length in Counter(circuits.values()).most_common(3)]
-    return largest_three[0] * largest_three[1] * largest_three[2]
-
-def solve_part2(p_input):
-    pass
+        else:
+            l2 = None
+        if l2 is not None:
+            last_two = l2
+    return circuits, next_circuit_id, last_two
