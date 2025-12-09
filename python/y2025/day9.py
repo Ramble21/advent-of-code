eec25dc82c75f17fb8953ba04426b0814816f2e4
Ramble21 <zramble21@gmail.com>
2025-12-09 14:43:35 -0500

    2025 day 9 part 1

---
+++ python/y2025/day9.py
@@ -0,0 +1,20 @@
+import math
+import re
+from python.helper_classes.common_methods import load_lines
+
+def parse_data():
+    strs = load_lines(2025, 9)
+    coord_tuples = list((int(x), int(y)) for line in strs for x, y in [re.split(",", line)])
+    return coord_tuples
+
+def solve_part1(p_input):
+    distances = []
+    for i in range(len(p_input)):
+        for j in range(i + 1, len(p_input)):
+            dx, dy = p_input[i][0] - p_input[j][0] + 1, p_input[i][1] - p_input[j][1] + 1
+            distances.append(abs(dx * dy))
+    distances.sort(reverse=True)
+    return distances[0]
+
+def solve_part2(p_input):
+    pass
